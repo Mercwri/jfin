4a3ee718874fa5de5115bbf94993aebedcd7daac
Mercwri <michaelell87@gmail.com>
2025-07-14 17:08:57 -0400

    Refactor ALB listener configuration and improve VPC resource formatting

--- alb.tf
+++ alb.tf
@@ -41,19 +41,19 @@ resource "aws_lb_target_group" "jf" {
   }
 }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = 80
-  protocol          = "HTTP"
-  default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
+# resource "aws_lb_listener" "http" {
+#   load_balancer_arn = aws_lb.lb.arn
+#   port              = 80
+#   protocol          = "HTTP"
+#   default_action {
+#     type = "redirect"
+#     redirect {
+#       port        = "443"
+#       protocol    = "HTTPS"
+#       status_code = "HTTP_301"
+#     }
+#   }
+# }
 
 resource "aws_lb_listener" "http_no_ssl" {
   load_balancer_arn = aws_lb.lb.arn
--- vpc.tf
+++ vpc.tf
@@ -7,16 +7,15 @@ resource "aws_vpc" "core" {
 
 resource "aws_internet_gateway" "core" {
   vpc_id = aws_vpc.core.id
+
 }
 
 resource "aws_route_table" "core" {
   vpc_id = aws_vpc.core.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.core.id
   }
-
 }
 
 resource "aws_subnet" "core" {
@@ -27,4 +26,10 @@ resource "aws_subnet" "core" {
   vpc_id            = aws_vpc.core.id
   cidr_block        = each.key
   availability_zone = "us-east-1${each.value}"
+}
+
+resource "aws_route_table_association" "core" {
+  for_each       = aws_subnet.core
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.core.id
 }
