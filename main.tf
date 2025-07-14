d24134f34cc19f9677bc53511979c836fee96d66
Mercwri <michaelell87@gmail.com>
2025-07-14 13:22:10 -0400

    Add S3 bucket resources and lifecycle configurations in main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,50 @@
 data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "media" {
+  bucket = "media-jf-mikeell"
+}
+
+resource "aws_s3_bucket" "backup" {
+  bucket = "backup-jf-mikeell"
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  for_each = toset([
+    aws_s3_bucket.media.id,
+    aws_s3_bucket.backup.id,
+  ])
+  bucket   = each.value
+  acl      = "private"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "itc" {
+  bucket = aws_s3_bucket.media.id
+  rule {
+    status = "Enabled"
+    id      = "itc"
+    expiration {
+      days = 365
+    }
+    transition {
+      days          = 0
+      storage_class = "INTELLIGENT_TIERING"
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "backup" {
+  bucket = aws_s3_bucket.backup.id
+  rule {
+    status = "Enabled"
+    id      = "backup"
+    expiration {
+      days = 30
+    }
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+}
