9d2330973303c260585fae71ce985e8a21eda127
Mercwri <michaelell87@gmail.com>
2025-07-16 11:06:45 -0400

    Remove SSH target group attachment and listener from ALB configuration

--- alb.tf
+++ alb.tf
@@ -68,19 +68,3 @@ resource "aws_lb_target_group_attachment" "jf" {
   target_id        = aws_instance.jellyfin.id
   port             = 80
 }
-
-resource "aws_lb_target_group_attachment" "jf-ssh" {
-  target_group_arn = aws_lb_target_group.jf.arn
-  target_id        = aws_instance.jellyfin.id
-  port             = 22
-}
-
-resource "aws_lb_listener" "ssh" {
-  load_balancer_arn = aws_lb.lb.arn
-  port              = 22
-  protocol          = "TCP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.jf.arn
-  }
-}
